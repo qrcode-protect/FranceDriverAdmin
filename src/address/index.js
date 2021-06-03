79d0b9a08803c198ae3da3f04ed24532f6fa83aa
qrcode-protect <dev@qrcode-protect.com>
2021-06-03 09:43:17 +0200

    new version resolve conflict

--- src/address/index.js
+++ src/address/index.js
@@ -6,11 +6,6 @@ import LocationCityIcon from "@material-ui/icons/LocationCity";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-<<<<<<< HEAD
-  list: AddressList,
-=======
-  //list: AddressList,
->>>>>>> 6473c82f5f3b4a8103c63d6a8ff5eec4d875fa4a
   show: AddressShow,
   edit: AddressEdit,
   icon: LocationCityIcon,
