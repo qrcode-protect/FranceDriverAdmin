dd01fdb39af5913ee79f6fa6fe74fa5132075e82
edouardportable <ed.dhalluin@gmail.com>
2021-06-03 10:24:56 +0200

    add avatar

--- src/customer/CustomerList.js
+++ src/customer/CustomerList.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { List, Datagrid, TextField, ReferenceField } from "react-admin";
+import { GetAvatarUrl } from "../helpers/helper";
 
 export const CustomerList = (props) => (
   <List {...props}>
-    <Datagrid>
+    <Datagrid rowClick="show">
+      <ReferenceField source="image" label="Avatar" reference="media_objects">
+        <GetAvatarUrl />
+      </ReferenceField>
       <TextField source="firstName" />
       <TextField source="lastName" />
       <ReferenceField source="address" reference="addresses" addLabel={false}>
         <TextField source="street" />
       </ReferenceField>
-      <TextField source="image" />
     </Datagrid>
   </List>
 );
