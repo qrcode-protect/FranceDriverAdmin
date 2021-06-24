f9592928416a6f82c3f9b8be64ffe143441de54f
qrcode-protect <dev@qrcode-protect.com>
2021-06-24 11:45:44 +0200

    update: file name

--- src/messageDriver/messageDriverList.js
+++ src/messageDriver/messageDriverList.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { List, Datagrid, TextField } from "react-admin";
 import { useFormik } from "formik";
-import { CustumTextField } from "../messagecustomer/CustumTextField";
+import { CustumTextField } from "../messageCustomer/CustumTextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
